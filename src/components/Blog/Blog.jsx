c78c4861a8fff4434d0f3f8ba8d8cb107983797e
Syed Mir Habib <mirakik.habib@gmail.com>
2023-05-05 20:05:22 +0600

    chore: blog update

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,66 +1,38 @@
-import React, { useRef } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import { useReactToPrint } from 'react-to-print';
+import React from 'react';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import PdfFile from './PdfFile';
 
-const Blog = () => {
+const Blogs = () => {
+return (
+<div className='container px-5 md:px-36'>
+<div className='p-5 md:p-10 py-10 md:py-16'>
+<div className='text-end mb-3'>
+<PDFDownloadLink document={<PdfFile></PdfFile>} fileName='blogs'>
+<button className="btn btn-outline">Download</button>
+</PDFDownloadLink>
+</div>
+{/* <PdfFile></PdfFile> */}
+<div className='row row-cols-1 row-cols-md-2 g-3'>
+<div className='col border p-3 md:p-5 rounded-lg bg-pink-100'>
+<p className='font-medium'>Differences between uncontrolled and controlled components?</p>
+<p className='mt-3'><span className='fw-bold'>Answer:</span> Which component manages its own state internally that is known as uncontrolled component. On the other hand controlled component is controlled by the parent component. In uncontrolled component while user interacts then its internal state updated directly. But controlled component controlled by calling function and using props.</p>
+</div>
+<div className='col border p-3 md:p-5 rounded-lg bg-pink-100'>
+<p className='font-medium'>How to validate React props using PropTypes?</p>
+<p className='mt-3'><span className='fw-bold'>Answer:</span>PropTypes is a package. Which usually validate react component's props. Developer can use it by installing via npm or yearn. If a component pass invalid type of props then PropTypes will throw and error and also console an alert.</p>
+</div>
+<div className='col border p-3 md:p-5 rounded-lg bg-pink-100'>
+<p className='font-medium'>Differences between nodejs and express js.</p>
+<p className=''><span className='fw-bold'>Answer: </span>Both tools used for web development though both of them have different purpose to complete. Node.js is actually is a runtime environment tools that run JavaScript code outside a browser. It makes platform for making web servers and also network applications. On top of that, Express.js is a web application framework. Main task of it is work as middleware for handling HTTP requests and responses. Also it control routing and template rendering.</p>
+</div>
+<div className='col border p-3 md:p-5 rounded-lg bg-pink-100'>
+<p className='font-medium'>What is a custom hook, and why will you create a custom hook?</p>
+<p className=''><span className='fw-bold'>Answer:</span>Mainly, custom hook is a JS function that uses React Hooks. It returns logic which can be shared through multiple components. For multiple purpose we can use custom hook. For example, 1. Not to repeat same code, 2. Can be used on encapsulating stateful logic.</p>
+</div>
+</div>
+</div>
+</div>
+);
+};
 
-    const componentRef = useRef();
-    const handlePrint = useReactToPrint({
-        content: () => componentRef.current,
-        documentTitle: "Blog Question & Answer",
-        onAfterPrint: () => toast("Data saved successfully")
-    });
-
-    return (
-        <div className='container'>
-            <div className='mt-4'>
-                <button className="btn btn-success" onClick={handlePrint}>Print this Article</button>
-            </div>
-            <div ref={componentRef} style={{ width: "100%", padding: "0px 16px" }}>
-                <h3 className='text-center my-3'>Text</h3>
-                <hr />
-                <div>
-                    <h4>Text</h4>
-                    <h5 className='ms-3'>Text: </h5>
-                    <ol type='1'>
-                        <li>Text</li>
-                        <li>Text</li>
-                        <li>Text</li>
-                    </ol>
-                    <h5 className='ms-3'>Text: </h5>
-                    <ol type='1'>
-                        <li>Text</li>
-                        <li>Text</li>
-                        <li>Text</li>
-                    </ol>
-                </div>
-
-                <div>
-                    <h4>Text</h4>
-                    <p>
-                        Text
-                    </p>
-                </div>
-
-                <div>
-                    <h4>Text</h4>
-                    <p>Text<br /><br />
-                        Text</p>
-                </div>
-
-                <div>
-                    <h4>Text</h4>
-                    <p>Text
-                        Text
-                        Text
-
-                        Text</p>
-                </div>
-                <hr />
-            </div>
-            <ToastContainer />
-        </div>
-    );
-}
-
-export default Blog;
+export default Blogs;
